5403ad0e9c6f01469a0eb534a21ca4ee19570d77
RinatG <rinat_galimov_1992@mail.ru>
2023-05-22 22:30:03 +0300

    Добавлен пользовательский интерфейс для метода getSalaryInfo в Main Внесены косметические изменения, удаление бесполезных частей программы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,19 +15,19 @@ public class Main {
     public static List<Company> companies = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Добро пожаловать в нашу программу" +
-                "\nЧто вы хотите сделать?" + "\n" +
-                "1. Создать компанию" + "\n" +
-                "2. Покинуть программу");
+        System.out.println("""
+                Добро пожаловать в нашу программу
+                Что вы хотите сделать?
+                1. Создать компанию
+                2. Покинуть программу""");
         String input = scanner.nextLine();
         int chose = Integer.parseInt(input);
         switch (chose) {
-            case 1:
-                newCompany();
-                break;
-            case 2:
+            case 1 -> newCompany();
+            case 2 -> {
                 System.out.println("Всего доброго");
                 System.exit(0);
+            }
         }
     }
 
@@ -48,13 +48,14 @@ public class Main {
             System.out.println(i + ". " + company.getName());
             i++;
         }
-        System.out.println("Что вы хотите сделать?" + "\n" +
-                "1. Сменить компанию" + "\n" +
-                "2. Создать новую компанию");
+        System.out.println("""
+                Что вы хотите сделать?
+                1. Сменить компанию
+                2. Создать новую компанию""");
         String input = scanner.nextLine();
         int chose = Integer.parseInt(input);
         switch (chose) {
-            case 1:
+            case 1 -> {
                 System.out.println("На данный момент у вас есть компании: ");
                 i = 0;
                 for (Company company : companies) {
@@ -66,65 +67,51 @@ public class Main {
                 int companyID = Integer.parseInt(input);
                 System.out.println("Теперь вы работаете с компанией " + companies.get(companyID).getName());
                 mainInterface(companies.get(companyID));
-                break;
-            case 2:
-                newCompany();
-                break;
-
+            }
+            case 2 -> newCompany();
         }
     }
 
     public static void mainInterface(Company company) {
         while (true) {
-            System.out.println("Основная программа" + "\n" +
-                    "Что вы хотите сделать?" + "\n" +
-                    "1. Добавить сотрудника" + "\n" +
-                    "2. Удалить сотрудника" + "\n" +
-                    "3. Узнать доход компании" + "\n" +
-                    "4. Узнать информацию о зарплатах компании" + "\n" +
-                    "5. Создать ещё одну компанию/Выбрать другую" + "\n" +
-                    "6. Добавить сотрудников списком");// Список заранее прописан в программе
+            System.out.println("""
+                    Основная программа
+                    Что вы хотите сделать?
+                    1. Добавить сотрудника
+                    2. Удалить сотрудника
+                    3. Узнать доход компании
+                    4. Узнать информацию о зарплатах компании
+                    5. Создать ещё одну компанию/Выбрать другую
+                    6. Добавить сотрудников списком""");// Список заранее прописан в программе
             String input = scanner.nextLine();
             if (input.replace(" ", "").equalsIgnoreCase("exit")) {
                 System.out.println("Всего доброго");
                 System.exit(0);
             }
             int chose = Integer.parseInt(input);
-            int count;
             switch (chose) {
-                case 1:
-                    addEmployee(company);
-                    break;
-                case 2:
-                    deleteEmployee(company);
-                    break;
-                case 3:
-                    System.out.println("Доход вашей компании: " + "\n" + company.getIncome());
-                    break;
-                case 4:
-                    getSalaryInfo(company);
-                    break;
-                case 5:
-                    choseCompany();
-                    break;
-                case 6:
+                case 1 -> addEmployee(company);
+                case 2 -> deleteEmployee(company);
+                case 3 -> System.out.println("Доход вашей компании: " + "\n" + company.getIncome());
+                case 4 -> getSalaryInfo(company);
+                case 5 -> choseCompany();
+                case 6 -> {
                     List<Employee> employeeToAdd = new ArrayList<>(Arrays.asList(new Manager(company),
                             new TopManager(company), new Operator(company),
                             new Manager(company), new Manager(company)));
                     company.hireAll(employeeToAdd);
-                    break;
-
+                }
             }
         }
     }
     public static void getSalaryInfo(Company company){
-        System.out.println("Что вы хотите сделать?" + "\n" +
-                "1. Узнать все уникальные зарплаты" + "\n" +
-                "2. Узнать самые высокие зарплаты"+ "\n" +
-                "3. Узнать самые низкие зарплаты");
+        System.out.println("""
+                Что вы хотите сделать?
+                1. Узнать все уникальные зарплаты
+                2. Узнать самые высокие зарплаты
+                3. Узнать самые низкие зарплаты""");
         String input = scanner.nextLine();
         int chose = Integer.parseInt(input);
-        System.out.println("");
         switch (chose){
             case 1:
                 for(Employee employee: company.getSalaryStaff()){
@@ -153,84 +140,77 @@ public class Main {
 
     }
     public static void deleteEmployee(Company company) {
-        System.out.println("Как вы хотите удалить сотрудника?" + "\n" +
-                "1. Удалить нескольких" + "\n" +
-                "2. Удалить по ID" + "\n" +
-                "3. Удалить любого");
+        System.out.println("""
+                Как вы хотите удалить сотрудника?
+                1. Удалить нескольких
+                2. Удалить по ID
+                3. Удалить любого""");
         String input = scanner.nextLine();
         int chose = Integer.parseInt(input);
         switch (chose) {
-            case 1:
-                System.out.println("Сколько сотрудников нужно удалить?" + "\n" +
-                        "1. По количеству" + "\n" +
-                        "2. По процентам");
-                input = scanner.nextLine();
-                int count = Integer.parseInt(input);
-                switch (count) {
-                    case 1:
-                        System.out.println("Введите кол-во сотрудников для удаления");
-                        input = scanner.nextLine();
-                        count = Integer.parseInt(input);
-                        while (count != 0 || company.employees.isEmpty()) {
-                            company.fire();
-                            count--;
-                        }
-                        System.out.println("Сотрудники удалены");
-                        mainInterface(company);
-                        break;
-                    case 2:
-                        System.out.println("Введите процент сотрудников для удаления");
-                        input = scanner.nextLine();
-                        count = Integer.parseInt(input);
-                        int i = company.employees.size() * count / 100;
-                        while (i != 0 || company.employees.isEmpty()) {
-                            company.fire();
-                            i--;
-                        }
-                        System.out.println("Сотрудники удалены");
-                        mainInterface(company);
-                        break;
-                }
-                if (count == 1) {
-                    while (count != 0 || company.employees.isEmpty()) {
-                        company.fire();
-                        count--;
-                    }
-                    System.out.println("Сотрудники удалены");
-                    mainInterface(company);
-                    break;
-                } else {
-
-                }
-
-            case 2:
+            case 1 -> deleteSomeEmployee(company);
+            case 2 -> {
                 System.out.println("Введите ID сотрудника в списке: ");
                 input = scanner.nextLine();
                 int id = Integer.parseInt(input);
                 company.fire(id);
                 mainInterface(company);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 company.fire();
                 System.out.println("Сотрудник удалён");
                 mainInterface(company);
-                break;
+            }
+        }
+
+    }
+    public static  void deleteSomeEmployee(Company company){
+        System.out.println("""
+                        Сколько сотрудников нужно удалить?
+                        1. По количеству
+                        2. По процентам""");
+        String input = scanner.nextLine();
+        int count = Integer.parseInt(input);
+        switch (count) {
+            case 1 -> {
+                System.out.println("Введите кол-во сотрудников для удаления");
+                input = scanner.nextLine();
+                count = Integer.parseInt(input);
+                while (count != 0 || company.employees.isEmpty()) {
+                    company.fire();
+                    count--;
+                }
+                System.out.println("Сотрудники удалены");
+                mainInterface(company);
+            }
+            case 2 -> {
+                System.out.println("Введите процент сотрудников для удаления");
+                input = scanner.nextLine();
+                count = Integer.parseInt(input);
+                int i = company.employees.size() * count / 100;
+                while (i != 0 || company.employees.isEmpty()) {
+                    company.fire();
+                    i--;
+                }
+                System.out.println("Сотрудники удалены");
+                mainInterface(company);
+            }
         }
 
     }
 
     public static void addEmployee(Company company) {
-        System.out.println("Какого сотрудника вы хотите добавить?" + "\n" +
-                "1. Manager" + "\n" +
-                "2. TopManager" + "\n" +
-                "3. Operator");
+        System.out.println("""
+                Какого сотрудника вы хотите добавить?
+                1. Manager
+                2. TopManager
+                3. Operator""");
         String input = scanner.nextLine();
         int chose = Integer.parseInt(input);
         int count;
 
         switch (chose) {
-            case 1:
-
+            case 1 -> {
                 System.out.println("Сколько сотрудников нужно добавить?");
                 input = scanner.nextLine();
                 count = Integer.parseInt(input);
@@ -241,9 +221,8 @@ public class Main {
                 }
                 System.out.println("Сотрудник(и) добавлен(ы)");
                 mainInterface(company);
-                break;
-            case 2:
-
+            }
+            case 2 -> {
                 System.out.println("Сколько сотрудников нужно добавить?");
                 input = scanner.nextLine();
                 count = Integer.parseInt(input);
@@ -254,8 +233,8 @@ public class Main {
                 }
                 System.out.println("Сотрудник(и) добавлен(ы)");
                 mainInterface(company);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 System.out.println("Сколько сотрудников нужно добавить?");
                 input = scanner.nextLine();
                 count = Integer.parseInt(input);
@@ -266,7 +245,7 @@ public class Main {
                 }
                 System.out.println("Сотрудник(и) добавлен(ы)");
                 mainInterface(company);
-                break;
+            }
         }
     }
 
